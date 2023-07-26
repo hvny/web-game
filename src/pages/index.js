8d18709e8bc4f4058ff96cb59fa067c3d801071f
Vadim Elsukov <elsukov.vadim@gmail.com>
2023-07-26 14:53:34 +0300

    dead end

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import {
     templatePlayerSelector,
     templateEnemySelector,
     templateContainerSelector,
+    gameZone,
     startButton,
     points,
     playerImage,
     enemyImage,
-    gameZone,
     music
 } from "../utils/constants.js"
 
@@ -18,10 +18,8 @@ let lifes = 4;
 
 const characterDmitry = new Player(templatePlayerSelector, playerImage); //создаём персонажа
 characterDmitry.generatePlayer();
-
 const playerElem = gameZone.querySelector(".player");
 
-const playerRect = playerElem.getBoundingClientRect();
 
 /*функция, которая прячет кнопку*/
 const hideStartButton = () => {     
@@ -61,19 +59,29 @@ const spawnGroupOfEnemies = () => {
         enemy.generateEnemy(enemyContainer);
     }
 
+    return { enemyContainer };
+
 };
 
-/*
-const minusLife = (playerRect, enemiesRect) => {
-    console.log(lifes);
-    if (enemiesRect.x <= 219 && playerRect.top < 528){
+const checkAlive = (player, enemy) => {
+    let playerBottom = parseInt(getComputedStyle(player).bottom);
+    let enemyLeft = parseInt(getComputedStyle(enemy).left);
+    //console.log(`player: ${playerBottom}\nenemy: ${enemyLeft}`);
+    console.log(`player: ${playerBottom}\nenemy: ${enemyLeft}`);
+    if (enemyLeft <= 90 && enemyLeft > 20 && playerBottom <= 50){
         lifes--;
+        console.log('lifes: ', lifes);
     }
-};*/
+};
 
 const spawnEnemies = () => {
     const spawn = setInterval(() => {
         spawnGroupOfEnemies();
+        /*
+        setInterval(() => {
+            checkAlive(playerElem, spawnGroupOfEnemies().enemyContainer);
+        }, 10);*/
+        
         if (!isGame) {
             clearInterval(spawn)
         }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,4 +8,5 @@ export const startButton = gameZone.querySelector(".game-zone__button");
 export const points = page.querySelector(".game-zone__points");
 export const playerImage = "../src/images/dmitry.jpg";
 export const enemyImage = "../src/images/max.jpg";
+
 export const music = new Audio("../src/audio/music.mp3");
